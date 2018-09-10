639ea4772b889c42b8e467991c7a3db0ad0dc9b0
Zhongxin Fu <zhxfu@mobvoi.com>
2018-09-10 10:41:03 +0800

    simple shape v2

--- simple_shapes.py
+++ simple_shapes.py
@@ -6,8 +6,8 @@ from PIL import Image
 import numpy as np
 
 def black2white_plus(size):
-    length = size 
-    width = size 
+    length = size
+    width = size
     # white background
     xy = np.ones((width, length, 3)) * 255
 
@@ -17,8 +17,8 @@ def black2white_plus(size):
     Image.fromarray(np.uint8(xy)).show()
 
 def black2white_multi(size):
-    length = size 
-    width = size 
+    length = size
+    width = size
     # white background
     xy = np.ones((width, length, 3)) * 255
 
@@ -29,7 +29,7 @@ def black2white_multi(size):
 
 def circle(size):
     length = size
-    width = size 
+    width = size
     # white background
     xy = np.ones((width, length, 3)) * 255
 
@@ -49,8 +49,8 @@ def circle(size):
     Image.fromarray(np.uint8(xy)).show()
 
 def ellipse(size):
-    length = size 
-    width = size 
+    length = size
+    width = size
     # white background
     xy = np.ones((width, length, 3)) * 255
 
@@ -69,11 +69,60 @@ def ellipse(size):
                 xy[x,y,:] = [0,0,255]
 
     Image.fromarray(np.uint8(xy)).show()
-    
+
+def circle_estimate(x, y, u):
+    r = 1
+    if abs((x**2 + y**2) - r) < 1/(u/2):
+        return True
+    else:
+        return False
+
+def ellipse_estimate(x, y, u):
+    a = 0.5
+    b = 1
+    if abs((x**2/a**2 + y**2/b**2) - 1) < 2/(u/2):
+        return True
+    else:
+        return False
+
+def hyperbola_estimate(x, y, u):
+    a = 0.5
+    b = 0.5
+#    if abs((x**2/a**2 - y**2/b**2) - 1) < 2/(u/2):
+    if abs((x**2/a**2 - y**2/b**2) + 1) < 2/(u/2):
+        return True
+    else:
+        return False
+
+def general_shapes(width=600,length=800,background=[255,255,255],frontground=[0,0,255],func=circle_estimate):
+
+    xy=np.ones((width, length, 3))
+    xy[:,:,:]=background
+
+    #x axis
+    xy[width//2,:,:]=frontground
+    #y axis
+    xy[:,length/2,:]=frontground
+
+    axis_unit=float(min(width, length))
+    for x in range(width):
+        for y in range(length):
+            axis_x=float(x-width/2)/(axis_unit/2)
+            axis_y=float(y-length/2)/(axis_unit/2)
+#            r = 1
+#            if abs((axis_x**2 + axis_y**2) - r) < 1/(float(axis_unit)/2):
+            if func(axis_x, axis_y, axis_unit):
+                xy[x,y,:]=frontground
+
+    Image.fromarray(np.uint8(xy)).show()
+
+
 if __name__ == '__main__':
     imagesize = 75*9
 #    black2white_multi(imagesize)
 #    black2white_plus(imagesize)
-    circle(imagesize)
+#    circle(imagesize)
 #    ellipse(imagesize)
-
+#    general_shapes(79*9,79*16,[204,232,207],[0,0,255],circle_estimate)
+#    general_shapes(79*9,79*16,[204,232,207],[0,0,255],ellipse_estimate)
+    general_shapes(79*9,79*16,[204,232,207],[0,0,255],hyperbola_estimate)
